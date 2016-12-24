ab5229176519095eb6fae44a9ac3dca1304f0d02
Marek <m.bojarovic@gmail.com>
2016-12-24 12:26:33 +0200

    Исправление ошибок Класса Db

--- 1/classes/Db.php
+++ 1/classes/Db.php
@@ -4,11 +4,11 @@ require __DIR__ . '/../config.php';
 
 class Db
 {
+    protected $dbh;
+
     public function __construct()
     {
         $dsn = DB_DSN;
-        $dbh = new PDO($dsn, DB_USER, DB_PASS);
-        return $dbh;
+        $this->dbh = new PDO($dsn, DB_USER, DB_PASS);
     }
-
 }
--- 1/index.php
+++ 1/index.php
@@ -3,4 +3,3 @@
 require __DIR__ . '/classes/Db.php';
 
 $db = new Db();
-//var_dump($db);
