644eaaf7b79ad4e099dfdd641ef7d359a2693e05
Marek <m.bojarovic@gmail.com>
2016-12-24 14:00:38 +0200

    Добавил method query() класса DB

--- 1/classes/Db.php
+++ 1/classes/Db.php
@@ -18,4 +18,12 @@ class Db
         return $sth->execute();
 
     }
+
+    public function query(string $sql, array $data=[])
+    {
+        $sth = $this->dbh->prepare($sql);
+        $sth->execute($data);
+        return $sth->fetchAll();
+
+    }
 }
--- 1/index.php
+++ 1/index.php
@@ -3,4 +3,9 @@
 require __DIR__ . '/classes/Db.php';
 
 $db = new Db();
-$db->execute('SELECT * FROM PERSONS');
+$data = $db->query(
+    'SELECT * FROM PERSONS WHERE age=:age',
+    [':age' => 12]
+);
+
+var_dump($data);
