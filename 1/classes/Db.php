69b1717bdaa001d0764ae57c985eb27be5de45cb
Marek <m.bojarovic@gmail.com>
2016-12-25 18:16:35 +0200

    Задание 2 выполнено

--- 1/classes/Db.php
+++ 1/classes/Db.php
@@ -1,6 +1,5 @@
 <?php
 
-
 class Db
 {
     protected $dbh;
--- 1/index.php
+++ 1/index.php
@@ -1,6 +1,7 @@
 <?php
 
 require __DIR__ . '/classes/Db.php';
+require __DIR__ . '/classes/View.php';
 
 $db = new Db();
 $data1 = $db->query(
